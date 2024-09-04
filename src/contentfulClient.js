7918c3bfea2f1a10dcc676fcc76d316809bf78f2
Yourgotopyromaniac <cipherx809@gmail.com>
2024-09-04 17:12:51 +0100

    hotfix: access token bugs

--- src/contentfulClient.js
+++ src/contentfulClient.js
@@ -2,7 +2,10 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: import.meta.env.VITE_SPACE_ID,
-  accessToken: import.meta.env.VITE_ACCESS_TOKEN
+  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 });
+
+
+
 
 export default client;
